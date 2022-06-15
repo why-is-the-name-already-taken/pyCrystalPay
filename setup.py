fe8ed9d11eeb9b08b7a5619c02c078a169c2397b
Shiro san <73769205+shirosandev@users.noreply.github.com>
2022-06-15 15:40:06 +0700

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(name='pycrystalpay',
-      version='2.2.2',
+      version='3.0',
       description='Lib for crystalpy.',
       packages=['pycrystalpay'],
-      url="https://github.com/seraphineRU/pyCrystalPay/",
-      long_description='doc - https://github.com/seraphineRU/pyCrystalPay/',
+      url="https://github.com/shirosandev/pyCrystalPay",
+      long_description='doc - https://github.com/shirosandev/pyCrystalPay',
       author_email='seraphineru@mail.ru',
       zip_safe=False)
